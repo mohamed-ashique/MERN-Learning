67308eb8380ccd83fee6e8f3b82a67c39759fbc9
mohamed-ashique <realashiquepk@gmail.com>
2026-06-14 12:57:37 +0400

    Add JavaScript variables and data types practice

---
+++ 03-javascript/day-24-js-basics/script.js
@@ -0,0 +1,39 @@
+console.log("Day 24 - JavaScript Basics");
+
+// String
+const fullName = "Mohamed Ashique";
+const city = "Dubai";
+const course = "MERN Stack";
+
+console.log(fullName);
+console.log(city);
+console.log(course);
+
+// Number
+const age = 25;
+const learningDays = 24;
+
+console.log(age);
+console.log(learningDays);
+
+// Boolean
+const isLearningJavaScript = true;
+const isCourseCompleted = false;
+
+console.log(isLearningJavaScript);
+console.log(isCourseCompleted);
+
+// let example
+let currentDay = 24;
+console.log(currentDay);
+
+currentDay = 25;
+console.log(currentDay);
+
+// undefined
+let futureJob;
+console.log(futureJob);
+
+// null
+let selectedProject = null;
+console.log(selectedProject);
